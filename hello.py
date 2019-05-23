52ffdc03f7012149cbda3c5551fde7242b925764
Isaac <isaac.r.watts@gmail.com>
2019-05-23 10:05:12 -0600

    notes for challenge creation

--- hello.py
+++ hello.py
@@ -3,9 +3,21 @@ print(msg)
 
 
 # Bot needs to Create Challenges
-# Party/guild ID
-# Name
-# Tag
-# Summary
-# Description
-# Categories
+# https://habitica.com/api/v3/challenges
+# challenge: Object
+# An object representing the challenge to be created
+
+# group: UUID
+# The id of the group to which the challenge belongs
+
+# name: String
+# The full name of the challenge
+
+# shortName: String
+# A shortened name for the challenge, to be used as a tag
+
+# challenge = {
+# 	"group": "UUID",
+# 	"name": "Foo",
+# 	"shortName": "Bar"
+# }
